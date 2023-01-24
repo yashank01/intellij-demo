1e6ebaf8b6ddc994a77fb7a7d4294adc9ebe2b87
yashank01 <yashank.garg@flipkart.com>
2023-01-24 14:27:11 +0530

    sts project first commit

---
+++ demoClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.flipkart.app;
+
+/**
+ * @author yashank.garg
+ *
+ */
+public class demoClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
